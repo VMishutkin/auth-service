a77f07669945525e0de45821823f1beea486e2f9
VMishutkin <vladmish@list.ru>
2023-02-09 07:45:04 +0700

    начал делать сущности

--- src/main/java/ru/skypro/homework/entity/Comment.java
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -1,4 +1,20 @@
 package ru.skypro.homework.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "comment")
 public class Comment {
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User user;
+    private String createdAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int pk;
+    private String text;
+
 }
--- src/main/java/ru/skypro/homework/entity/User.java
+++ src/main/java/ru/skypro/homework/entity/User.java
@@ -1,4 +1,30 @@
 package ru.skypro.homework.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private LocalDate regDate;
+    private String city;
+
+    @OneToMany(mappedBy = "user")
+    private List<Comment> comments;
+    @OneToMany(mappedBy = "author")
+    private List<Comment> ads;
+    @Lob
+    private byte[] image;
+
 }
